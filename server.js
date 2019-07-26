8c100be2ab41287e3a69cc796b0deb42f067a79d
Jose M Rico <jrico@princeton.edu>
2019-07-25 20:32:49 -0400

    https

--- server.js
+++ server.js
@@ -10,31 +10,25 @@ const PORT_HTTP = 7070;
 const PORT_HTTPS = 7171;
 const app = express();
 
-/*
-const privateKey = fs.readFileSync(
-	"/etc/letsencrypt/live/kapricornmedia.com-0001/privkey.pem", "utf8");
-const cert = fs.readFileSync(
-	"/etc/letsencrypt/live/kapricornmedia.com-0001/cert.pem", "utf8");
-const ca = fs.readFileSync(
-	"/etc/letsencrypt/live/kapricornmedia.com-0001/chain.pem", "utf8");
+const privateKey = fs.readFileSync("./keys/privkey.pem", "utf8");
+const cert = fs.readFileSync("./keys/cert.pem", "utf8");
+const ca = fs.readFileSync("./keys/chain.pem", "utf8");
 
 const credentials = {
 	key: privateKey,
 	cert: cert,
 	ca: ca
 };
-*/
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "/public")));
 
 const httpServer = http.createServer(app);
-//const httpsServer = https.createServer(credentials, app);
+const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(PORT_HTTP, function() {
 	console.log("HTTP server listening on port " + PORT_HTTP);
 });
-/*httpsServer.listen(PORT_HTTPS, function() {
+httpsServer.listen(PORT_HTTPS, function() {
 	console.log("HTTPS server listening on port " + PORT_HTTPS);
 });
-*/
