e0d967badf3393052c2fd51e00bb8e3f53c7bfc9
Jose M Rico <jrico@princeton.edu>
2019-07-25 20:04:13 -0400

    updated ports and stuff

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const bodyParser = require("body-parser");
-const cson = require("cson");
 const express = require("express");
 const fs = require("fs");
 const http = require("http");
@@ -7,10 +6,11 @@ const https = require("https");
 const path = require("path");
 const util = require("util");
 
-const PORT_HTTP = 8080;
-const PORT_HTTPS = 8181;
+const PORT_HTTP = 7070;
+const PORT_HTTPS = 7171;
 const app = express();
 
+/*
 const privateKey = fs.readFileSync(
 	"/etc/letsencrypt/live/kapricornmedia.com-0001/privkey.pem", "utf8");
 const cert = fs.readFileSync(
@@ -23,52 +23,18 @@ const credentials = {
 	cert: cert,
 	ca: ca
 };
+*/
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "/public")));
 
-// Serve post entries
-app.post("/posts", function(req, res) {
-	var MAX_POSTS = 20;
-
-	var type = req.query.type;
-	if (typeof(type) !== "string") {
-		res.status(400);
-		res.send("Type argument invalid");
-		return;
-	}
-
-	var names = req.body;
-	if (!Array.isArray(names) || names.length > MAX_POSTS) {
-		res.status(400);
-		res.send("Names not an array or larger than "
-			+ MAX_POSTS + " elements");
-		return;
-	}
-
-	var result = [];
-	for (var i = 0; i < names.length; i++) {
-		var postFilePath = path.join(__dirname, "/posts", type,
-			names[i] + ".cson");
-		try {
-			var postData = cson.parse(fs.readFileSync(postFilePath));
-		} catch (e) {
-			res.status(400);
-			res.send("Requested non-existent file: " + postFilePath);
-			return;
-		}
-		result.push(postData);
-	}
-
-	res.send(result);
-});
-
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+//const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(PORT_HTTP, function() {
 	console.log("HTTP server listening on port " + PORT_HTTP);
 });
-httpsServer.listen(PORT_HTTPS, function() {
+/*httpsServer.listen(PORT_HTTPS, function() {
 	console.log("HTTPS server listening on port " + PORT_HTTPS);
 });
+*/
