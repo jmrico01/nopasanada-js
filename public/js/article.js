93f26dc23c94ee2d9b5857a37752b7a7e8baa204
Jose M Rico <jrico@princeton.edu>
2019-08-09 00:27:38 -0400

    fix article page

--- public/js/article.js
+++ public/js/article.js
@@ -1,11 +1,5 @@
 "use strict";
 
-function OnResize() {
-    let headerHeight = $("#header").height();
-    $(".screen").css("padding-top", headerHeight);
-    $(".screen").css("padding-bottom", headerHeight);
-}
-
 window.onload = function() {
     OnResize();
     $("#content").css("visibility", "visible");
--- public/js/main.js
+++ public/js/main.js
@@ -110,31 +110,6 @@ function HandleHash(hash)
     $("#screen2").show();
 }
 
-function OnResize() {
-    let headerHeight = $("#header").height();
-    $(".screen").each(function(index) {
-        let $this = $(this);
-        let thisHeight = $this.height();
-        let topSpacing = headerHeight;
-        let bottomSpacing = document.documentElement.clientHeight - thisHeight - topSpacing;
-        $this.css("padding-top", topSpacing);
-        $this.css("padding-bottom", bottomSpacing);
-    })
-
-    //$("#screen2").css("padding-top", 0);
-
-    let aspect = document.documentElement.clientWidth / document.documentElement.clientHeight;
-    if (aspect < 1.45 && !warned) {
-        warned = true;
-        setTimeout(function() {
-            alert("WARNING\n\n"
-                + "You are viewing the website at a thin aspect ratio (less than 1.5).\n"
-                + "Layout and spacing will be messed up until we explicitly design\n"
-                + "for these taller formats (tall windows / mobile)");
-        }, 0);
-    }
-}
-
 window.onhashchange = function() {
     let hash = window.location.hash;
     if (hash !== prevHash) {
