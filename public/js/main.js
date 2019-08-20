51757b2bb46b60a21072e9ae34f3d0342b6854cd
Jose M Rico <jmrico01@gmail.com>
2019-08-20 00:50:08 -0400

    tomela

--- public/js/main.js
+++ public/js/main.js
@@ -83,21 +83,33 @@ const ARTICLES = {
     "trailerp": {
         video: "51aG1gGsULU",
         title: "Pilotos, Episodio 1<br>Trailer",
+        subtitle: "",
+        author: "",
+        date: "",
         text: "<p><em>&ldquo;Familia&rdquo; o &ldquo;C&oacute;mo ganar en Enduro por m&aacute;s de 50 a&ntilde;os&rdquo; es el primer episodio de nuestra serie Pilotos.</em></p> <p><em>Aqu&iacute; conocemos a una familia en Costa Rica ha triunfado en el deporte de Enduro por m&aacute;s de 50 a&ntilde;os. Esta es su historia.</em></p> <p>Pilotos comienza el 23 de Agosto.</p>"
     },
     "trailere": {
         video: "g36TGvzIi5o",
         title: "Enfoque, Episodio 1<br>Trailer",
+        subtitle: "",
+        author: "",
+        date: "",
         text: "<p><em>En Enfoque conocemos las vidas e historias de celebrados y nuevos artistas viviendo y trabajando en Latinoam&eacute;rica.</em></p>"
     },
     "articulo1": {
         image: "guilasexual1.jpg",
-        title: "El caso Diet Prada<br>y el abuso sexual en el mundo de la moda.",
+        title: "El caso <em>Diet Prada</em> y el<br>abuso sexual en el<br>mundo de la <u>moda</u>.",
+        subtitle: "El negocio turbio detr&aacute;s de la fama<br>y sus fot&oacute;grafos",
+        author: "POR: PAULINA JOARISTI",
+        date: "AGOSTO 19",
         text: "<p>El acoso y las relaciones de poder en el mundo de la moda est&aacute;n lejos de ser un secreto, gracias en parte a la ola de denuncias que ha incentivado el movimiento #metoo. El caso Diet Prada deja estas conductas a&uacute;n m&aacute;s en evidencia: desde hace unos a&ntilde;os, la reconocida cuenta de instagram existe para exponer c&oacute;mo los famosos y dise&ntilde;adores se copian entre ellos, pero se volvi&oacute; viral cuando ayud&oacute; a un grupo significativo de mujeres a denunciar a sus acosadores.</p> <p>Queda demostrado adem&aacute;s que, aunque el abusador es culpable, la industria tambi&eacute;n lo es: las agencias de modelaje, los c&oacute;mplices que reciben alg&uacute;n tipo de ganancia monetaria, etc. La cultura del mundo de la moda no tiene l&iacute;mites establecidos ni oficinas de recursos humanos donde los modelos pueden acudir a pedir ayuda, y pone a los modelos en situaciones que atentan contra su dignidad y su cuerpo para poder alcanzar sus metas.</p> <p>Diet Prada, que se mantiene anónima por temas de seguridad, empezó por denunciar a <a href=\"https://people.com/style/kim-kardashian-photographer-marcus-hyde-accused-of-soliciting-nude-photos-from-model/\">Marcus Hyde</a>, fotógrafo conocido por trabajar con Kim Kardashian y Ariana Grande. Según las tomas de pantalla que publicaron las víctimas, el hombre <a href=\"https://www.dazeddigital.com/fashion/article/45406/1/marcus-hyde-timur-emek-photogaphers-accused-sexual-misconduct-harassment-assault\">ofrecía ayudarlas</a> con su carrera a cambio de fotos desnudas, con la excusa de que necesitaba ver sus cuerpos y determinar si merecía invertir su tiempo en ellas. Las que se negaban a cooperar eran insultadas por Hyde; las que proporcionaban las fotos eran invitadas a una sesión fotográfica donde eran manoseadas y presionadas a tener sexo.</p> <p>Lo curioso es que hay un proceso de <a href=\"https://www.usmagazine.com/celebrity-news/news/kim-kardashian-reacts-to-photographer-marcus-hyde-allegations/\">encubrimiento</a> de las actuaciones del fotógrafo por parte de famosos. Se sabe por ejemplo, que la cuenta de Instagram de Marcus Hyde - la cuál contaba con más de un millón de seguidores - ha sido borrada, y que tanto Kim Kardashian como Ariana Grande han emitido comunicados indicando simpatizar con las víctimas y alabar la decisión de hablar públicamente sobre sus experiencias - aunque no sin detenerse a recalcar que, en lo personal, nunca fueron sujetas de acoso por parte del señor Hyde.</p> <p>Las revelaciones en torno a Hyde propiciaron que unos días después salieran nuevas acusaciones, pero esta vez en contra de Timur Emek. Emek era bien conocido por ser el fotógrafo de las modelos de Victoria Secret. Al igual que en el caso anterior, Emek se aprovechó de su posición para solicitar sexo a cambio de viajes, fama y posicionamiento en la industria del modelaje.</p> <p>Acá algunas de las imágenes que las víctimas hicieron públicas.</p> <p>Caption: En las fotos de arriba podemos observar como Hyde le dice a la modelo que sus photoshoots cuestan dos mil dólares, pero que si ella estuviera dispuesta a hacerlo desnuda lo haría de gratis.</p> <p>Los modelos hombres también pueden ser víctimas.</p> <p>El acoso sexual afecta tanto a la mujer como el hombre en cualquier industria, cultura o país, y el mundo de la moda no es excepción. Bruce Weber y Mario Testino, fotógrafos que en su momento fueron los más importantes de la reconocida revista Vogue, fueron también acusados por su comportamiento y reemplazados de forma definitiva, luego de que varios modelos - en este caso hombres -  los denunciaran por abusar sexualmente de ellos. Ambos negaron los cargos presentados alegando que los acusadores no pueden ser <a href=\"https://www.harpersbazaar.com/uk/fashion/fashion-news/a15156038/mario-testino-accused-of-sexual-assault-by-multiple-models/\">tomados en serio</a>, pero la editora de Vogue, Anna Wintour, se mantuvo firme y dijo que aunque mantiene una relación de amistad con ambos profesionales, no puede ignorar acusaciones como estas.</p> <p>La revista The Cut hizo una serie de entrevistas a víctimas masculinas de acoso sexual que revelan que los hombres también enfrentan estigmas al hablar sobre el abuso; muchos llegan a cuestionarse los hechos al ser confrontados al respecto. ¿Por qué no se defendieron? es una pregunta común. ¿No pudieron haber simplemente golpeado a sus abusadores? Esto crea un ciclo de vergüenza y culpa que impide a las víctimas denunciar.</p> <p>Aunque empresas como Condé Nast han implementado nuevos <a href=\"https://www.vogue.com/article/anna-wintour-responds-mario-testino-bruce-weber-sexual-misconduct-allegations\">códigos de conducta</a> después de haber entrevistado a modelos, estilistas, agencias, fotografos, maquillistas y encargados de sets, cambios significativos en la cultura aún deben suceder para que las víctimas puedan empoderarse y relatar sus experiencias. Imaginen los miles de casos como estos que no han salido a la luz por dinero, poder o influencia… de no ser por el movimiento #metoo, quizá no nos habríamos enterado de ninguno.</p>"
     },
     "video": {
         video: "TzxV2HgY35s",
         title: "No Pasa Nada:<br>El porqu&eacute;.",
+        subtitle: "",
+        author: "",
+        date: "",
         text: "<p>La incertidumbre es una de nuestras respuestas m&aacute;s humanas. Aparece ante eventos inesperados, o momentos que quiebran nuestra rutina. Adem&aacute;s, la incertidumbre es tan cordial que no s&oacute;lo nos acompa&ntilde;a cuando no tenemos control e intentamos buscar orden, pero adem&aacute;s, cuando tenemos control y buscamos hacer algo diferente.</p> <p>La incertidumbre es ese sentimiento que nos abruma con impotencia, nos confunde, nos pesa como yugo de madera vieja en los hombros y nos quita el aire en ese espacio hueco entre nuestras costillas, arriba del estomago y debajo del coraz&otilde;n.</p> <p>La incertidumbre es miedo, es ansiedad, es emoci&oacute;n y felicidad. Es lo que sentimos al entender que vivimos nuestro primer amor, y que ese mismo se acab&oacute;. La incertidumbre es el no saber qu&eacute; va a pasar.</p> <p>Ante la incertidumbre siempre va a existir una respuesta. La respuesta nos encara con nuestra naturaleza, nos hace accionar sobre lo que somos. Ese momento de acci&oacute;n con el cual nos enfrentamos ante lo que se interpuso entre nosotros y lo incierto; nos analiza, nos juzga, nos hace lo que somos, y nos prepara para lo que vamos a ser.</p> <p>Lo que nos define no es no es la situaci&oacute;n que se interpone, ni el sentimiento previo a esta, ni tampoco sus consecuencias absolutas e incambiables. Lo que nos define s&oacute;lo existe en el hacer, porque en un mundo de constantes intangibles podemos s&oacute;lo decir que somos eso que realmente hacemos.</p> <p>Es entonces ese espacio, ese hueco entre nuestras costillas, arriba del estomago y debajo del coraz&oacute;n que se llena en grande de incertidumbre en el antes del hacer, que nace: &ldquo;No Pasa Nada.&rdquo;</p> <p>Es una voz que habla con amor y responsabilidad para decir:</p> <p><em>&ldquo;Anda amigo, tranquilo, no pasa nada.&rdquo;</em></p>"
     }
 };
@@ -207,6 +219,9 @@ function SetArticle(articleName)
     let article = ARTICLES[articleName];
 
     $("#articleTitle").html(article.title);
+    $("#articleSubtitle").html(article.subtitle);
+    $("#articleAuthor").html(article.author);
+    $("#articleDate").html(article.date);
     $("#articleText").html(article.text);
 
     if (article.hasOwnProperty("video")) {
