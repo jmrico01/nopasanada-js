07dc939af517de0a3cf93daabc268aec3649a97f
Jose M Rico <jmrico01@gmail.com>
2019-08-20 11:40:35 -0400

    https

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ dbConnection.query("SELECT * FROM raw", function(error, results, fields) {
     // console.log(fields);
 });
 
-/*const privateKey = fs.readFileSync("./keys/privkey.pem", "utf8");
+const privateKey = fs.readFileSync("./keys/privkey.pem", "utf8");
 const cert = fs.readFileSync("./keys/cert.pem", "utf8");
 const ca = fs.readFileSync("./keys/chain.pem", "utf8");
 
@@ -44,17 +44,17 @@ const credentials = {
 	key: privateKey,
 	cert: cert,
 	ca: ca
-};*/
+};
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "/public")));
 
 const httpServer = http.createServer(app);
-//const httpsServer = https.createServer(credentials, app);
+const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(PORT_HTTP, function() {
 	console.log("HTTP server listening on port " + PORT_HTTP);
 });
-/*httpsServer.listen(PORT_HTTPS, function() {
+httpsServer.listen(PORT_HTTPS, function() {
 	console.log("HTTPS server listening on port " + PORT_HTTPS);
-});*/
+});
