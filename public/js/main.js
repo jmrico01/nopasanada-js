a853b0aa1fd1d53faed2a820a209a1033d3c2095
Jose M Rico <jmrico01@gmail.com>
2019-08-20 02:18:45 -0400

    slightly better phone

--- public/js/main.js
+++ public/js/main.js
@@ -374,7 +374,9 @@ function OnResize() {
         $("#featuredText").hide();
         $(".entry").css("width", "25vw");
         $("#articleTitle").css("font-size", "24pt");
+        $("#articleTitle").css("line-height", "24pt");
         $("#articleSubtitle").css("font-size", "20pt");
+        $("#articleSubtitle").css("line-height", "20pt");
         $("#articleSubtext").css("font-size", "10pt");
         $("#articleText p").css("font-size", "16pt");
         $("#articleText p").css("line-height", "16pt");
@@ -382,6 +384,9 @@ function OnResize() {
         $(".entryText").css("font-size", "3vw");
         $(".entryText").css("line-height", "3vw");
         $(".entryText").css("margin-bottom", "15vw");
+        $("#color1").css("display", "none");
+        $("#color2").css("display", "none");
+        $("")
     }
     else {
         $("#headerCategories").show();
@@ -391,13 +396,17 @@ function OnResize() {
         $("#featuredText").show();
         $(".entry").css("width", "15.625vw");
         $("#articleTitle").css("font-size", "5.4vw");
+        $("#articleTitle").css("line-height", "6.25vw");
         $("#articleSubtitle").css("font-size", "3.4vw");
+        $("#articleSubtitle").css("line-height", "3.9vw");
         $("#articleSubtext").css("font-size", "1vw");
         $("#articleText p").css("font-size", "1.4vw");
         $("#articleText p").css("line-height", "2.2vw");
         $(".entryText").css("font-size", "0.95vw");
         $(".entryText").css("line-height", "1.6vw");
         $(".entryText").css("margin-bottom", "0");
+        $("#color1").css("display", "block");
+        $("#color2").css("display", "block");
     }
 }
 
