a5cc7cc1f659e0ec7cd4128000dc20239fd083c2
Jose M Rico <jmrico01@gmail.com>
2019-09-13 17:14:19 -0600

    lo importante 13'

--- public/js/main.js
+++ public/js/main.js
@@ -8,14 +8,14 @@ const DEFAULT_CATEGORY = "noticias";
 const ENTRIES_FEATURED = {
     "noticias": {
         images: [
-            "201909/headers/12-newsletter.jpg"
+            "201909/headers/13-lo-importante.jpg"
         ],
         pretitle: "NOTICIAS:",
-        title: "LO IMPORTANTE<br><b>12-09-19. *</b>",
+        title: "LO IMPORTANTE<br><b>13-09-19. *</b>",
         decoration: "",
         text1: "4 HISTORIAS IMPORTANTES DE LAS M&Aacute;S RECIENTES NOTICIAS INTERNACIONALES.",
         text2: "CURADAS POR<br>NO PASA NADA",
-        link: "/content/201909/12-newsletter",
+        link: "/content/201909/13-lo-importante",
         highlightcolor: "#ff613c"
     },
     "cultura": {
