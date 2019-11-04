ad62714543da47d49ad2728001e30e5d23f7e8dd
Jose M Rico <jmrico01@gmail.com>
2019-11-04 09:35:13 -0500

    no article yet

--- data/globalData.js
+++ data/globalData.js
@@ -1,8 +1,9 @@
 exports.featured = {
-    "home": "/content/201911/04-sociedad",
+    // "home": "/content/201911/04-sociedad",
+    "home": "/content/201911/04-lo-importante",
     "noticias": "/content/201911/04-lo-importante",
     "ciencia": "/content/201910/29-campeon-ambiental",
-    "cultura": "/content/201911/04-sociedad",
+    "cultura": "/content/201910/31-costa-rica-vs-el-cine",
     "opinion": "/content/201910/31-costa-rica-vs-el-cine",
     "deporte": "/content/201908/pilotos-trailer",
     "nopasanada": "/content/201908/nopasanada"
