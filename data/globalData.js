f10e9d4bfe3a52d5eac05e057e1c83afb0d2bea4
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-11-08 19:04:34 +0000

    backend, Fri Nov 08 2019 19:04:34 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,8 +1,8 @@
 exports.featured = {
-    "home": "/content/201911/08-bojack-horseman",
+    "home": "/content/201911/08-lo-importante",
     "noticias": "/content/201911/08-lo-importante",
     "ciencia": "/content/201910/29-campeon-ambiental",
-    "cultura": "/content/201911/08-bojack-horseman",
+    "cultura": "/content/201910/31-costa-rica-vs-el-cine",
     "opinion": "/content/201911/04-sociedad",
     "deporte": "/content/201908/pilotos-trailer",
     "nopasanada": "/content/201908/nopasanada"
