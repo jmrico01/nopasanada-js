8831dd1de53be390312c1e2ea0058d24d9a499ab
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-11-08 19:04:17 +0000

    backend, Fri Nov 08 2019 19:04:17 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,9 +1,9 @@
 exports.featured = {
-    "home": "/content/201911/06-lo-importante",
-    "noticias": "/content/201911/06-lo-importante",
-    "ciencia": "/content/201911/04-sociedad",
-    "cultura": "/content/201910/31-costa-rica-vs-el-cine",
-    "opinion": "/content/201910/31-costa-rica-vs-el-cine",
+    "home": "/content/201911/08-bojack-horseman",
+    "noticias": "/content/201911/08-lo-importante",
+    "ciencia": "/content/201910/29-campeon-ambiental",
+    "cultura": "/content/201911/08-bojack-horseman",
+    "opinion": "/content/201911/04-sociedad",
     "deporte": "/content/201908/pilotos-trailer",
     "nopasanada": "/content/201908/nopasanada"
 };
