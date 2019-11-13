abc1f779be4a417ab3cf7f4a16cad0afb5d07e76
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-11-13 16:28:51 +0000

    backend, Wed Nov 13 2019 16:28:51 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,6 +1,6 @@
 exports.featured = {
     "home": "/content/201911/13-ceviches",
-    "noticias": "/content/201911/12-lo-importante",
+    "noticias": "/content/201911/13-lo-importante",
     "ciencia": "/content/201910/29-campeon-ambiental",
     "cultura": "/content/201911/13-ceviches",
     "opinion": "/content/201911/04-sociedad",
