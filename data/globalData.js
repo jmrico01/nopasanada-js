93cd6ca3d9732d17a6a40af157be60104aa95809
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-11-15 18:40:07 +0000

    backend, Fri Nov 15 2019 18:40:07 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,5 +1,5 @@
 exports.featured = {
-    "home": "/content/201911/14-manual-para-el-cine-de-fantasia-latinoamericano",
+    "home": "/content/201911/15-andrea-kader",
     "noticias": "/content/201911/15-lo-importante",
     "ciencia": "/content/201910/29-campeon-ambiental",
     "cultura": "/content/201911/14-manual-para-el-cine-de-fantasia-latinoamericano",
