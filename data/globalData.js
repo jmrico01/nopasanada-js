fe519019f811d3bc7541fcc3e290a433e1201f64
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-11-16 14:32:42 +0000

    backend, Sat Nov 16 2019 14:32:42 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,6 +1,6 @@
 exports.featured = {
-    "home": "/content/201911/15-andrea-kader",
-    "noticias": "/content/201911/15-lo-importante",
+    "home": "/content/201911/16-lo-importante",
+    "noticias": "/content/201911/16-lo-importante",
     "ciencia": "/content/201910/29-campeon-ambiental",
     "cultura": "/content/201911/14-manual-para-el-cine-de-fantasia-latinoamericano",
     "opinion": "/content/201911/04-sociedad",
