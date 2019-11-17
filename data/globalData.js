64843f640e2bbbc0369fa1c4d2179c462e85fe35
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-11-17 02:46:57 +0000

    backend, Sun Nov 17 2019 02:46:57 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,6 +1,6 @@
 exports.featured = {
-    "home": "/content/201911/16-lo-importante",
-    "noticias": "/content/201911/16-lo-importante",
+    "home": "/content/201911/16-de-nov",
+    "noticias": "/content/201911/16-de-nov",
     "ciencia": "/content/201910/29-campeon-ambiental",
     "cultura": "/content/201911/14-manual-para-el-cine-de-fantasia-latinoamericano",
     "opinion": "/content/201911/04-sociedad",
