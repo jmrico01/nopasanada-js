1014d453720a91cc6a1cea63b65aa9aaf1178b30
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-11-25 17:14:24 +0000

    backend, Mon Nov 25 2019 17:14:24 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,6 +1,6 @@
 exports.featured = {
-    "home": "/content/201911/21-en-venezuela-lo-importante",
-    "noticias": "/content/201911/21-en-venezuela-lo-importante",
+    "home": "/content/201911/25-lo-importante",
+    "noticias": "/content/201911/25-lo-importante",
     "ciencia": "/content/201910/29-campeon-ambiental",
     "cultura": "/content/201911/19-lado-b",
     "opinion": "/content/201911/04-sociedad",
