6d3677729d170a19e578b8cd1d4e6c5c3c0b5bd8
Jose M Rico <jmrico01@gmail.com>
2019-11-30 18:12:16 -0500

    vimeo embed

--- server.js
+++ server.js
@@ -658,6 +658,12 @@ app.get("/content/*/*", async function(req, res) {
             }
         },
         {
+            tag: "vimeo",
+            content: function(arg, style, entryMedia) {
+                return "<div style=\"padding:56.25% 0 0 0;position:relative;\"><iframe src=\"https://player.vimeo.com/video/" + arg + "?title=0&byline=0&portrait=0\" style=\"position:absolute;top:0;left:0;width:100%;height:100%;\" frameborder=\"0\" allow=\"autoplay; fullscreen\" allowfullscreen></iframe></div><script src=\"https://player.vimeo.com/api/player.js\"></script>";
+            }
+        },
+        {
             tag: "instagram",
             content: function(arg, style, entryMedia) {
                 return "<blockquote class=\"instagram-media\" data-instgrm-permalink=\"https://www."
