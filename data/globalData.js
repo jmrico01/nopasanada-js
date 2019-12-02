13043b1dce9fbc1172d8b6dddd1435562ccb56cb
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-12-02 15:26:14 +0000

    backend, Mon Dec 02 2019 15:26:14 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,5 +1,5 @@
 exports.featured = {
-    "home": "/content/201912/02-ocean-cleanup",
+    "home": "/content/201911/30-tristeza",
     "noticias": "/content/201911/29-lo-importante",
     "ciencia": "/content/201912/02-ocean-cleanup",
     "cultura": "/content/201911/30-tristeza",
