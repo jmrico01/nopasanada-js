1e1cec26b6322e0dda1861cdd616e9e92dae2b5b
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-12-05 18:37:14 +0000

    backend, Thu Dec 05 2019 18:37:13 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,6 +1,6 @@
 exports.featured = {
     "home": "/content/201912/05-woke-brands",
-    "noticias": "/content/201912/05-lo-importante",
+    "noticias": "/content/201912/05-trump-impeachment",
     "ciencia": "/content/201912/02-ocean-cleanup",
     "cultura": "/content/201912/04-los-goyas",
     "opinion": "/content/201912/05-woke-brands",
