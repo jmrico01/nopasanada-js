a669f23dc86098db01bc7db087e7924de3747bb4
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-12-06 16:08:03 +0000

    backend, Fri Dec 06 2019 16:08:02 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,8 +1,8 @@
 exports.featured = {
-    "home": "/content/201912/05-woke-brands",
+    "home": "/content/201912/05-himno-feminista",
     "noticias": "/content/201912/05-trump-impeachment",
     "ciencia": "/content/201912/02-ocean-cleanup",
-    "cultura": "/content/201912/04-los-goyas",
+    "cultura": "/content/201912/05-himno-feminista",
     "opinion": "/content/201912/05-woke-brands",
     "deporte": "/content/201908/pilotos-trailer",
     "nopasanada": "/content/201908/nopasanada"
