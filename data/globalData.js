e6cadd862dfa2f418dc1c00dc249e21847ae876b
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-12-09 15:43:53 +0000

    backend, Mon Dec 09 2019 15:43:53 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,8 +1,8 @@
 exports.featured = {
-    "home": "/content/201912/05-himno-feminista",
+    "home": "/content/201912/09-gallo-pinto",
     "noticias": "/content/201912/06-lo-importante",
     "ciencia": "/content/201912/02-ocean-cleanup",
-    "cultura": "/content/201912/05-himno-feminista",
+    "cultura": "/content/201912/09-gallo-pinto",
     "opinion": "/content/201912/05-woke-brands",
     "deporte": "/content/201908/pilotos-trailer",
     "nopasanada": "/content/201908/nopasanada"
