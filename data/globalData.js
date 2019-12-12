d8da4ada601a6a278c51001a104e99e1914a03a8
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-12-12 20:24:12 +0000

    backend, Thu Dec 12 2019 20:24:12 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,6 +1,6 @@
 exports.featured = {
     "home": "/content/201912/09-al-mercat",
-    "noticias": "/content/201912/11-lo-importante",
+    "noticias": "/content/201912/12-lo-importante",
     "ciencia": "/content/201912/02-ocean-cleanup",
     "cultura": "/content/201912/09-al-mercat",
     "opinion": "/content/201912/05-woke-brands",
