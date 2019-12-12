8f8deccb49b0a2ba13f636843802759863edbcb5
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-12-12 23:06:13 +0000

    backend, Thu Dec 12 2019 23:06:13 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,6 +1,6 @@
 exports.featured = {
-    "home": "/content/201912/12-gustav-klimt",
-    "noticias": "/content/201912/12-lo-importante",
+    "home": "/content/201912/12-solo-lo-importante-video",
+    "noticias": "/content/201912/12-solo-lo-importante-video",
     "ciencia": "/content/201912/02-ocean-cleanup",
     "cultura": "/content/201912/12-gustav-klimt",
     "opinion": "/content/201912/05-woke-brands",
