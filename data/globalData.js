eaf00ab4d5d77bee41bb838f095f55da8cd7f941
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-12-17 21:15:58 +0000

    backend, Tue Dec 17 2019 21:15:58 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -4,6 +4,6 @@ exports.featured = {
     "ciencia": "/content/201912/02-ocean-cleanup",
     "cultura": "/content/201912/17-anna-karina",
     "opinion": "/content/201912/17-anna-karina",
-    "deporte": "/content/201908/pilotos-trailer",
+    "deporte": "/content/201912/17-me-mejor-experiencia",
     "nopasanada": "/content/201908/nopasanada"
 };
