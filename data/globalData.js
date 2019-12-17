3e60cb8778e7c7dcf7e8c1e5055896f38b15d825
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-12-17 16:31:08 +0000

    backend, Tue Dec 17 2019 16:31:07 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,6 +1,6 @@
 exports.featured = {
     "home": "/content/201912/13-desayunos",
-    "noticias": "/content/201912/16-lo-importante",
+    "noticias": "/content/201912/17-lo-importante",
     "ciencia": "/content/201912/02-ocean-cleanup",
     "cultura": "/content/201912/12-gustav-klimt",
     "opinion": "/content/201912/13-beatles-y-satanismo",
