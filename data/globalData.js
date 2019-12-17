f8ad6cd4e80eed30c201d55271893fad7dde2c99
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-12-17 17:27:24 +0000

    backend, Tue Dec 17 2019 17:27:24 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,9 +1,9 @@
 exports.featured = {
-    "home": "/content/201912/13-desayunos",
+    "home": "/content/201912/17-anna-karina",
     "noticias": "/content/201912/17-lo-importante",
     "ciencia": "/content/201912/02-ocean-cleanup",
-    "cultura": "/content/201912/12-gustav-klimt",
-    "opinion": "/content/201912/13-beatles-y-satanismo",
+    "cultura": "/content/201912/17-anna-karina",
+    "opinion": "/content/201912/17-anna-karina",
     "deporte": "/content/201908/pilotos-trailer",
     "nopasanada": "/content/201908/nopasanada"
 };
