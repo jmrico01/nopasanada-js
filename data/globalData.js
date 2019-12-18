df750c5a50efb54b11c8c4c0a4e612632b0642f8
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2019-12-18 14:38:31 +0000

    backend, Wed Dec 18 2019 14:38:31 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -2,7 +2,7 @@ exports.featured = {
     "home": "/content/201912/18-lo-importante",
     "noticias": "/content/201912/18-lo-importante",
     "ciencia": "/content/201912/02-ocean-cleanup",
-    "cultura": "/content/201912/17-anna-karina",
+    "cultura": "/content/201912/13-desayunos",
     "opinion": "/content/201912/17-anna-karina",
     "deporte": "/content/201912/17-me-mejor-experiencia",
     "nopasanada": "/content/201908/nopasanada"
