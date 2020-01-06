4f11b816414796e02f2ce6f91db560a84f01f06d
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2020-01-06 17:37:20 +0000

    backend, Mon Jan 06 2020 17:37:20 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,8 +1,8 @@
 exports.featured = {
-    "home": "/content/202001/04-bush-fires",
+    "home": "/content/202001/06-golden-globes",
     "noticias": "/content/202001/qasem-soleimani",
     "ciencia": "/content/202001/04-bush-fires",
-    "cultura": "/content/201912/13-desayunos",
+    "cultura": "/content/202001/06-golden-globes",
     "opinion": "/content/201912/17-anna-karina",
     "deporte": "/content/201912/17-me-mejor-experiencia",
     "nopasanada": "/content/201908/nopasanada"
