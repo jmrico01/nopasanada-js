2ce179426e2e00db6a69870cdae95bd1dab50342
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2020-01-07 20:34:07 +0000

    backend, Tue Jan 07 2020 20:34:07 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,6 +1,6 @@
 exports.featured = {
-    "home": "/content/202001/06-golden-globes",
-    "noticias": "/content/202001/qasem-soleimani",
+    "home": "/content/202001/07-lo-importante",
+    "noticias": "/content/202001/07-lo-importante",
     "ciencia": "/content/202001/04-bush-fires",
     "cultura": "/content/202001/06-golden-globes",
     "opinion": "/content/201912/17-anna-karina",
