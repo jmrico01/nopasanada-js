746b74d647475b3695bdde034b4f543abaf6a0ca
Backend <jmrico01@gmail.com>
2020-01-08 18:51:46 +0000

    backend, Wed Jan 08 2020 18:51:46 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -2,7 +2,7 @@ exports.featured = {
     "home": "/content/202001/08-lo-importante",
     "noticias": "/content/202001/07-lo-importante",
     "ciencia": "/content/202001/04-bush-fires",
-    "cultura": "/content/202001/06-golden-globes",
+    "cultura": "/content/202001/08-herencia-cultural-de-iran",
     "opinion": "/content/201912/17-anna-karina",
     "deporte": "/content/201912/17-me-mejor-experiencia",
     "nopasanada": "/content/201908/nopasanada"
