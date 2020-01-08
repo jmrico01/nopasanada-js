e83d705dc14a0405b35ea9a4896c3081be169221
Ubuntu <ubuntu@ip-172-31-26-202.us-east-2.compute.internal>
2020-01-08 16:40:49 +0000

    backend, Wed Jan 08 2020 16:40:49 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,5 +1,5 @@
 exports.featured = {
-    "home": "/content/202001/que-paso-venezuela",
+    "home": "/content/202001/08-lo-importante",
     "noticias": "/content/202001/07-lo-importante",
     "ciencia": "/content/202001/04-bush-fires",
     "cultura": "/content/202001/06-golden-globes",
