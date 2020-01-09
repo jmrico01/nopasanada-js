fcca97a1f32f2ef0c8ff155e364d62dc8b9dc8ed
Backend <jmrico01@gmail.com>
2020-01-09 16:38:55 +0000

    backend, Thu Jan 09 2020 16:38:54 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -2,7 +2,7 @@ exports.featured = {
     "home": "/content/202001/09-lo-importante",
     "noticias": "/content/202001/necesita-saber-iran",
     "ciencia": "/content/202001/04-bush-fires",
-    "cultura": "/content/202001/08-herencia-cultural-de-iran",
+    "cultura": "/content/202001/09-hildur-guonadottir",
     "opinion": "/content/201912/lo-mejor-2019",
     "deporte": "/content/201912/17-me-mejor-experiencia",
     "nopasanada": "/content/201908/nopasanada"
