29d5a940566c6430dfe4cecf553e898ee6eae066
Backend <jmrico01@gmail.com>
2020-01-15 17:54:28 +0000

    backend, Wed Jan 15 2020 17:54:28 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,7 +1,7 @@
 exports.featured = {
-    "home": "/content/202001/menu-perfecto-ep1",
+    "home": "/content/202001/15-camaras-trampa",
     "noticias": "/content/202001/13-lo-importante",
-    "ciencia": "/content/202001/04-bush-fires",
+    "ciencia": "/content/202001/15-camaras-trampa",
     "cultura": "/content/202001/09-hildur-guonadottir",
     "opinion": "/content/202001/09-hildur-guonadottir",
     "deporte": "/content/201912/17-me-mejor-experiencia",
