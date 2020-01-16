2a4c6733f1e37a3e591018ddb047109bfe1524b8
Backend <jmrico01@gmail.com>
2020-01-16 15:10:43 +0000

    backend, Thu Jan 16 2020 15:10:43 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -2,7 +2,7 @@ exports.featured = {
     "home": "/content/202001/15-camaras-trampa",
     "noticias": "/content/202001/15-lo-importante",
     "ciencia": "/content/202001/15-camaras-trampa",
-    "cultura": "/content/202001/09-hildur-guonadottir",
+    "cultura": "/content/202001/15-oscars",
     "opinion": "/content/202001/09-hildur-guonadottir",
     "deporte": "/content/201912/17-me-mejor-experiencia",
     "nopasanada": "/content/201908/nopasanada"
