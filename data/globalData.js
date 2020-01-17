011d6959c25017ea075a8550060f159d1b32bc91
Backend <jmrico01@gmail.com>
2020-01-17 15:37:33 +0000

    backend, Fri Jan 17 2020 15:37:33 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,8 +1,8 @@
 exports.featured = {
-    "home": "/content/202001/15-oscars",
+    "home": "/content/202001/17-musee-paris",
     "noticias": "/content/202001/16-lo-importante",
     "ciencia": "/content/202001/15-camaras-trampa",
-    "cultura": "/content/202001/15-oscars",
+    "cultura": "/content/202001/17-musee-paris",
     "opinion": "/content/202001/09-hildur-guonadottir",
     "deporte": "/content/201912/17-me-mejor-experiencia",
     "nopasanada": "/content/201908/nopasanada"
