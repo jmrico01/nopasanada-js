34238a3589795cdc3946b2b68a2780a3ad090447
Backend <jmrico01@gmail.com>
2020-01-20 17:03:11 +0000

    backend, Mon Jan 20 2020 17:03:11 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,7 +1,7 @@
 exports.featured = {
     "home": "/content/202001/17-musee-paris",
-    "noticias": "/content/202001/17-lo-importante",
-    "ciencia": "/content/202001/15-camaras-trampa",
+    "noticias": "/content/202001/20-lo-importante",
+    "ciencia": "/content/202001/20-supernova-betelgeuse",
     "cultura": "/content/202001/17-musee-paris",
     "opinion": "/content/202001/09-hildur-guonadottir",
     "deporte": "/content/201912/17-me-mejor-experiencia",
