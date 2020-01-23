85da6cfd9daf463d33db64c5d9b1529ae3656784
Backend <jmrico01@gmail.com>
2020-01-23 21:11:35 +0000

    backend, Thu Jan 23 2020 21:11:35 GMT+0000 (UTC)

--- data/globalData.js
+++ data/globalData.js
@@ -1,8 +1,8 @@
 exports.featured = {
-    "home": "/content/202001/21-parasite",
+    "home": "/content/202001/23-gaultier",
     "noticias": "/content/202001/23-lo-importante",
     "ciencia": "/content/202001/20-supernova-betelgeuse",
-    "cultura": "/content/202001/21-parasite",
+    "cultura": "/content/202001/23-gaultier",
     "opinion": "/content/202001/09-hildur-guonadottir",
     "deporte": "/content/201912/17-me-mejor-experiencia",
     "nopasanada": "/content/201908/nopasanada"
